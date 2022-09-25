b439217998a8a127d05d13c323e975c9c60f47c4
fydkz <herajyulianti88@gmail.com>
2022-09-25 19:36:32 +0700

    initial commit awal

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert("pppp")
+var nama = prompt("masukan nama")
+
+
+
+
+window.open = function() {};
+ window.print = function() {};
+// Support hover state for mobile.
+if (false) {
+    window.ontouchstart = function() {};
+ }
